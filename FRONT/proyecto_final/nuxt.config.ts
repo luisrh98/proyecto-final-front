eb219fa43931a9ee622d380d43246152b791333a
Luis Rodriguez Hernandez <lrodher@g.educaand.es>
2025-04-30 15:08:13 +0200

    Añadido panel de admin predeterminado de Django (Sin acabar) e idea de panel de admin front

--- FRONT/proyecto_final/nuxt.config.ts
+++ FRONT/proyecto_final/nuxt.config.ts
@@ -17,10 +17,8 @@ export default defineNuxtConfig({
   runtimeConfig: {
     stripeSecretKey: process.env.STRIPE_SECRET_KEY, // Disponible solo en el servidor
     public: {
-      //API_BASE_URL: process.env.API_BASE_URL || 'https://proyecto-tienda-59128ee34d3c.herokuapp.com/api',
-      //API_BASE_URL: process.env.API_BASE_URL || 'https://proyecto-final-back-xir2.onrender.com/api',
-      //API_BASE_URL: process.env.API_BASE_URL || 'https://proyecto-final.duckdns.org/api',
-      API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:8000/api',
+      API_BASE_URL: process.env.API_BASE_URL || 'https://proyecto-final.duckdns.org/api',
+      //API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:8000/api',
       stripePublicKey: process.env.NUXT_PUBLIC_STRIPE_PUBLIC_KEY // Disponible en el cliente
     }
   },
