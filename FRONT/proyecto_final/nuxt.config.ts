f2794ba41e9d8d96d10bbe1bb031dee5efa6a66c
Luis Rodriguez Hernandez <lrodher@g.educaand.es>
2025-05-05 15:15:23 +0200

    Cambiado url a produccion

--- FRONT/proyecto_final/nuxt.config.ts
+++ FRONT/proyecto_final/nuxt.config.ts
@@ -4,8 +4,8 @@ export default defineNuxtConfig({
   runtimeConfig: {
     stripeSecretKey: process.env.STRIPE_SECRET_KEY, // Disponible solo en el servidor
     public: {
-      //API_BASE_URL: process.env.API_BASE_URL || 'https://proyecto-final.duckdns.org/api',
-      API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:8000/api',
+      API_BASE_URL: process.env.API_BASE_URL || 'https://proyecto-final.duckdns.org/api',
+      //API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:8000/api',
       stripePublicKey: process.env.NUXT_PUBLIC_STRIPE_PUBLIC_KEY // Disponible en el cliente
     }
   },
